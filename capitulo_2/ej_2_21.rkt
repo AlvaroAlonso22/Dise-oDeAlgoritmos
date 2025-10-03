e4ca3d0518c444a90b47c19b6539bab280ec753c
dcr909 <dcr909@alumnos.unican.es>
2025-10-03 17:34:45 +0200

    Ejercicio 21 subido

---
+++ capitulo_2/ej_2_21.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(define (square-list items)
+  (if (null? items)
+      '()
+      (cons (* (car items) (car items))
+            (square-list (cdr items)))))
+
+(define (square-list items)
+  (map (lambda (x) (* x x)) items))
