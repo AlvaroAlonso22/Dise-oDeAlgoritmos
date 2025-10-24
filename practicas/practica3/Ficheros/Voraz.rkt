aae962482111ccdb5b63b1c606f8cbc45d586939
AlvaroAlonso22 <alvaro.alonsoha@alumnos.unican.es>
2025-10-24 17:29:55 +0200

    Ejer 3 pract 3

--- practicas/practica3/Ficheros/Voraz.rkt
+++ practicas/practica3/Ficheros/Voraz.rkt
@@ -22,6 +22,45 @@
   ; Complete todo el código que se necesite.
   ; Al final, debe devolver una lista de listas con las 
   ; habilidades de los individuos seleccionados.
+  
+  ;;Función auxiliar para sumar habilidades
+  (define (suma lst)
+    (if (null? lst)
+        0
+        (+ (car lst) (suma (cdr lst)))))
+
+  ;;Función que obtiene el elemento con la mayor suma
+  (define (maximo-por-suma lista)
+    (if (null? lista)
+        '()
+        (let loop ((resto (cdr lista))
+                   (mejor (car lista))
+                   (max-suma (suma (car lista))))
+          (if (null? resto)
+              mejor
+              (let* ((actual (car resto))
+                     (s (suma actual)))
+                (if (> s max-suma)
+                    (loop (cdr resto) actual s)
+                    (loop (cdr resto) mejor max-suma)))))))
+    
+   ;;Funcion que elimina la primera aparición de un elemento en la lista
+  (define (eliminar elem lista)
+    (cond
+      ((null? lista) '())
+      ((equal? (car lista) elem) (cdr lista))
+      (else (cons (car lista) (eliminar elem (cdr lista))))))
+
+  ;;Bucle principal voraz
+  (let loop ((restantes datos)
+             (seleccionados '()))
+    (if (or (null? restantes)
+            (= (length seleccionados) 20))
+        (reverse seleccionados)
+        (let* ((mejor (maximo-por-suma restantes))
+               (nuevos-restantes (eliminar mejor restantes)))
+          (loop nuevos-restantes (cons mejor seleccionados)))))
+        
 )
 
 (length (Parcial-voraz entrada)) ; deberia devolver 20
